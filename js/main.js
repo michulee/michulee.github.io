c0cc235bf42c5dc15736b3afcf3d7b42dccbff52
Michelle Lee <michellechulee@gmail.com>
2020-11-11 01:38:08 -0800

    i'm done and i won't come back

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Current Year for Footer
+var d = new Date(); page.currentYear = d.getFullYear(); 
+
 // https://stackoverflow.com/a/50279663/7799687
 // Toggle down arrow icon
 window.addEventListener('scroll', function () {
@@ -8,3 +11,34 @@ window.addEventListener('scroll', function () {
     }
     this.oldScroll = this.scrollY;
 });
+
+let slides = document.querySelectorAll('slide-item');
+
+// detect left or right button
+// keep track of count slide index
+
+// let slideIndex = 0;
+// document.getElementById('btn-left').addEventListener('click', function() {
+//     slideIndex--;
+// });
+// document.getElementById('btn-right').addEventListener('click', function() {
+//     slideIndex++;
+// });
+
+let slideIndex = 0;
+
+document.getElementById('btn-left').addEventListener('click', setSlideIndex);
+document.getElementById('btn-right').addEventListener('click', setSlideIndex);
+
+function setSlideIndex() {
+    slideIndex++;
+}
+
+function getSlideIndex() {
+    return slideIndex;
+}
+setSlideIndex();
+console.log(getSlideIndex(slideIndex));
+
+// learn how to to use events and event.target
+// find more on different ways to do click event listeners
