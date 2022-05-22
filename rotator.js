e9c527402d7a33934000f784f2edcd5ce9730995
Michelle Lee <michellechulee@gmail.com>
2022-05-22 03:56:47 -0700

    theme colors, navigations, rotator script

---
+++ rotator.js
@@ -0,0 +1,119 @@
+// GLOBAL VARIABLES
+let currentID = 0;
+let previousID = 0;
+let isNextSlide = false;
+
+// DOM ELEMENTS
+const imagesList = document.querySelector("#rotator_image").children;
+const circleNavList = document.querySelector("#project_circle_nav").children;
+const hoverLeftNav = document.querySelector("#rotator_hover_left");
+const hoverRightNav = document.querySelector("#rotator_hover_right");
+
+// EVENT LISTENER - IMAGE NAVIGATION
+hoverLeftNav.addEventListener("click", () => {
+    // modular components - can have both or just by itself
+    prevSlide();
+    prevCircle();
+
+    // reset
+    isNextSlide = false;
+})
+hoverRightNav.addEventListener("click", () => {
+    // modular components - can have both or just by itself
+    nextSlide();
+    nextCircle();
+
+    // reset
+    isNextSlide = false;
+})
+
+// EVENT LISTENER - CIRCLE NAVIGATION
+for(let e of circleNavList) {
+    e.addEventListener("click", () => {
+        // modular components - can have both or just by itself
+        nextSlide();
+        nextCircle();
+    
+        // reset
+        isNextSlide = false;
+    })
+
+}
+
+// IMAGE NAVIGATION
+function nextSlide() {
+    // make active slide disappear
+    const activeSlide = document.querySelector("#rotator_image li.active");
+    activeSlide.classList.toggle("active");
+
+    // iterate slide and make it appear
+    if(isNextSlide) {
+        imagesList[currentID].classList.toggle("active");
+    } else {
+        if(currentID === imagesList.length-1) {
+            currentID = 0;
+        } else {
+            currentID++;
+        }
+        imagesList[currentID].classList.toggle("active");
+    }
+    isNextSlide = true;
+}
+function prevSlide() {
+    // make active slide disappear
+    const activeSlide = document.querySelector("#rotator_image li.active");
+    activeSlide.classList.toggle("active");
+
+    // iterate slide and make it appear
+    if(isNextSlide) {
+        imagesList[currentID].classList.toggle("active");
+    } else {
+        if(currentID === previousID) {
+            currentID = imagesList.length-1;
+        } else {
+            currentID--;
+        }
+        imagesList[currentID].classList.toggle("active");
+    }
+    isNextSlide = true;
+}
+
+// CIRCLE NAVIGATION
+function nextCircle() {
+    // make active button disappear
+    const activeButton = document.querySelector("#project_circle_nav li.active");
+    activeButton.classList.toggle("active");
+
+    // iterate button and change active
+    if(isNextSlide) {
+        circleNavList[currentID].classList.toggle("active");
+    } else {
+        if(currentID === previousID) {
+            currentID = 0;
+        } else {
+            currentID++;
+        }
+        circleNavList[currentID].classList.toggle("active");
+    }
+    isNextSlide = true;
+}
+function prevCircle() {
+    // make active button disappear
+    const activeButton = document.querySelector("#project_circle_nav li.active");
+    activeButton.classList.toggle("active");
+
+    // iterate button and change active
+    if(isNextSlide) {
+        circleNavList[currentID].classList.toggle("active");
+    } else {
+        if(currentID === previousID) {
+            currentID = imagesList.length-1;
+        } else {
+            currentID--;
+        }
+        circleNavList[currentID].classList.toggle("active");
+    }
+    isNextSlide = true;
+}
+
+
--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-//TODO
-/**
- * - hover, see right and left buttons
- * - buttons on left and right of slider
- * - bottom circle nav for projects
- */
+// only one rocket may appear depending on scroll position
+window.onscroll = () => {
+    var scrollPos = document.getElementsByTagName("body")[0].scrollTop;
+    // scroll position 15 is slightly past the hero banner section
+    if(scrollPos >= 115) {
+        document.getElementById("btn_middle_container").style.opacity = "0";
+
+        document.getElementById("btn_top_container").style.opacity = "1";
+        document.getElementById("btn_top_container").style.height = "auto";
+    } else {
+        document.getElementById("btn_middle_container").style.opacity = "1";
+        document.getElementById("btn_middle_container").style.height = "auto";
+
+        document.getElementById("btn_top_container").style.opacity = "0";
+    }
+};
