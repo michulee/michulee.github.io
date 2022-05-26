23c03b32f80ef734605c8f40d79b430909655524
Michelle Lee <michellechulee@gmail.com>
2022-05-25 22:58:23 -0700

    image, responsiveness

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // only one rocket may appear depending on scroll position
 window.onscroll = () => {
     var scrollPos = document.getElementsByTagName("body")[0].scrollTop;
-    // scroll position 15 is slightly past the hero banner section
+    // scroll position 115 is slightly past the hero banner section
     if(scrollPos >= 115) {
         document.getElementById("btn_middle_container").style.opacity = "0";
 
+        document.getElementById("btn_top_container").style.display = "block";
         document.getElementById("btn_top_container").style.opacity = "1";
         document.getElementById("btn_top_container").style.height = "auto";
     } else {
         document.getElementById("btn_middle_container").style.opacity = "1";
         document.getElementById("btn_middle_container").style.height = "auto";
 
-        document.getElementById("btn_top_container").style.opacity = "0";
+        document.getElementById("btn_top_container").style.display = "none";
+        // document.getElementById("btn_top_container").style.opacity = "0";
     }
 };
 
